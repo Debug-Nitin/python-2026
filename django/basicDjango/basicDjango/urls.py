a143ee230ba40cbe978224efbc18f5f0485a2915
nitin_singh <singhnitin2108@gmail.com>
2026-06-04 18:53:41 +0530

    added auto reload functionality and applied admin migrations

--- django/basicDjango/basicDjango/urls.py
+++ django/basicDjango/basicDjango/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('apps/', include('basicApp.urls')),
+
+    
+    path('__reload__/', include('django_browser_reload.urls')),
 ]
