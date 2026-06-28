730f826dc394399b21ee058bd7f19336ea472deb
nitin_singh <singhnitin2108@gmail.com>
2026-06-28 18:38:16 +0530

    added a model for the app

--- django/basicDjango/basicApp/models.py
+++ django/basicDjango/basicApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class appvariety(models.Model):
+    APP_TYPE_CHOICE = [
+        ('Fruit', 'Fruit'),
+        ('Vegetable', 'Vegetable'),
+        ('Grain', 'Grain'),
+        ('Legume', 'Legume'),
+        ('Nut', 'Nut'),
+        ('Herb', 'Herb'),
+        ('Spice', 'Spice')
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='basics/')
+    date_added = models.DateTimeField(default = timezone.now)
+    type = models.CharField(max_length=10 ,choices=APP_TYPE_CHOICE)
